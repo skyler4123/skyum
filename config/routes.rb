79bbf474dda5d14f4d4d546cafe0e404aefb7341
skyler <pkphung2001@gmail.com>
2023-09-21 14:07:55 +0700

    Add route for session

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
   resources :categories
   resources :users
   resources :customers
+  get "signin", to: 'sessions#signin'
+  post  "signin",to: 'sessions#create'
+  delete "signout", to: 'sessions#destroy'
+  get "signup", to: "sessions#signup"
+  post "signup", to: "users#create"
   # Defines the root path route ("/")
   root "users#index"
 end
